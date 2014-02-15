0956069bbd87b5c40f14c289f72fe4095fb1f481
graffio-m <graffio@lynxlab.com>
2014-02-15 23:44:32 +0100

    utility for training import see commit: 747e20d02db66326db36f62cc6d8824eaeb060a5

---
+++ modules/proRoma/include/utility.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+class utility {
+
+    function __construct() {
+        
+    }
+
+    /**
+     * @abstract return the code of words
+     * @param string $words
+     * @return string $istatCode
+     * 
+     */
+    public static function getIstatCode($words) {
+        
+        $urlSemanticApi = URL_LAVORI4.$words;
+        $keywords = $words;
+        //$curlHeader = array("Content-Type: application/x-www-form-urlencoded");
+        $curlHeader = '';
+        $codeResult = REST_request::sendRequest($keywords,$curlHeader,$urlSemanticApi,$curlPost);
+        $codeResult = json_decode($codeResult,TRUE);
+        $istatCode = $codeResult['job_types']['categories'][0]['category'];        
+//        return $jobsCode['job_types']['categories'][0]['category'];
+        return $istatCode;
+    }
+}
+
